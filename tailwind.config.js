56ef352c462c8994b52516fe429a715b1c3a8f95
Fokkio <fok04052@gmail.com>
2023-10-02 17:19:25 +0700

    change font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require('tailwindcss/defaultTheme')
+
 
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
       fontFamily: {
-        'sans': ['8BIT WONDER', defaultTheme.fontFamily.sans],
+        '8bit': ['8bit', 'sans-serif']
       },
     },
     plugins: [],
